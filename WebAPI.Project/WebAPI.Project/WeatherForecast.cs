e63b6c459f96c8adf6e9e2624a440654711e9858
Rushikesh Shinde <110456956+rushinshinde@users.noreply.github.com>
2025-07-16 16:08:16 +0530

    Changed weather forecast

--- WebAPI.Project/WebAPI.Project/WeatherForecast.cs
+++ WebAPI.Project/WebAPI.Project/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace WebAPI.Project
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? Summary1 { get; set; }
+
     }
 }
