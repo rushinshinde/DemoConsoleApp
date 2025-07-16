ee1100e83f7162b389ba9fde21d58697c635eec0
Rushikesh Shinde <110456956+rushinshinde@users.noreply.github.com>
2025-07-16 16:55:48 +0530

    Grapes Details Added

---
+++ WebAPI.Project/WebAPI.Project/Controllers/GrapesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using WebAPI.Project.Model;
+
+
+namespace WebAPI.Project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GrapesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/grapesDeatils")]
+        public Grapes GetGrapesData()
+        {
+            return new Grapes()
+            {
+                Name = "shenen",
+                Color = "purpule",
+                Variety = "New"
+            };
+        }
+
+        [HttpGet]
+        [Route("/grapeList")]
+
+        public List<Grapes> GetGrapesList() {
+            return new List<Grapes>()
+            {
+                new Grapes() {
+                    Name = "shenen",
+                    Color = "purpule",
+                    Variety = "New"
+                },
+                new Grapes() {
+                    Name = "shardeno",
+                    Color = "white",
+                    Variety = "juici"
+                },
+                new Grapes(){
+                    Name = "Ara-15",
+                    Color = "black",
+                    Variety = "white"
+                }
+
+            };
+        }
+    }
+}
+
---
+++ WebAPI.Project/WebAPI.Project/Model/Grapes.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace WebAPI.Project.Model
+{
+    public class Grapes
+    {
+        public String Name { get; set; }
+        public String Color { get; set; }
+        public String Variety { get; set; }
+    }
+}
